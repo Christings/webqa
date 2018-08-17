d50222226dba2a649438527b860f61a1b76f1370
zjjfx88 <zjj7842806@163.com>
2018-08-17 13:09:06 +0800

    add urls

---
+++ webqo/urls.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'zhangjingjun'
+__mtime__ = '2018/8/6'
+# ----------Dragon be here!----------
+              ┏━┓      ┏━┓
+            ┏━┛ ┻━━━━━━┛ ┻━━┓
+            ┃       ━       ┃
+            ┃  ━┳━┛   ┗━┳━  ┃
+            ┃       ┻       ┃
+            ┗━━━┓      ┏━━━━┛
+                ┃      ┃神兽保佑
+                ┃      ┃永无BUG！
+                ┃      ┗━━━━━━━━━┓
+                ┃                ┣━┓
+                ┃                ┏━┛
+                ┗━━┓ ┓ ┏━━━┳━┓ ┏━┛
+                   ┃ ┫ ┫   ┃ ┫ ┫
+                   ┗━┻━┛   ┗━┻━┛
+"""
+from django.urls import re_path
+from . import views
+app_name = 'webqo'
+urlpatterns = [
+    # webqo
+    # re_path(r'^debug/$', views.debug),
+    # re_path(r'^auto/$', views.auto),
+]
---
+++ wiki/urls.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'zhangjingjun'
+__mtime__ = '2018/8/6'
+# ----------Dragon be here!----------
+              ┏━┓      ┏━┓
+            ┏━┛ ┻━━━━━━┛ ┻━━┓
+            ┃       ━       ┃
+            ┃  ━┳━┛   ┗━┳━  ┃
+            ┃       ┻       ┃
+            ┗━━━┓      ┏━━━━┛
+                ┃      ┃神兽保佑
+                ┃      ┃永无BUG！
+                ┃      ┗━━━━━━━━━┓
+                ┃                ┣━┓
+                ┃                ┏━┛
+                ┗━━┓ ┓ ┏━━━┳━┓ ┏━┛
+                   ┃ ┫ ┫   ┃ ┫ ┫
+                   ┗━┻━┛   ┗━┻━┛
+"""
+from django.urls import re_path
+from . import views
+app_name = 'wiki'
+urlpatterns = [
+    # wiki
+    # re_path(r'^edit/$', views.edit),
+    # re_path(r'^list/$', views.list),
+]
