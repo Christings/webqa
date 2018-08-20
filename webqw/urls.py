56a11f354dbae845f7c34cb6db65595faa7a4a5d
Gladysgong <531404376@qq.com>
2018-08-20 11:39:31 +0800

    webqw--dubug

--- webqw/urls.py
+++ webqw/urls.py
@@ -25,6 +25,16 @@ from . import views
 app_name = 'webqw'
 urlpatterns = [
     # webqw
-    # re_path(r'^debug/$', views.debug),
+    re_path(r'^debug/$', views.debug),
+    # re_path(r'^qw_automation(?P<page_id>\d*)$',views.qw_automation),
+    # re_path(r'^qw_automation_add$',views.qw_automation_add),
+    # re_path(r'^qw_task_detail_(?P<task_id>\d+).html$',views.qw_task_detail),
+    # re_path(r'^qw_task_readd$',views.qw_task_readd),
+    # re_path(r'^qw_task_cancel',views.qw_task_cancel),
+    # re_path(r'^qw_req$',views.qw_req),
+    # re_path(r'^qw_req_info$',views.qw_req_info),
+    re_path(r'^debug_save',views.debug_save),
+    re_path(r'^debug_del',views.debug_del),
+    re_path(r'^debug_diff$',views.debug_diff),
     # re_path(r'^auto/$', views.auto),
 ]
