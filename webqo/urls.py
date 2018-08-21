990fb0f272d715f35da3c86a609919d92c9ea077
Gladysgong <531404376@qq.com>
2018-08-21 14:57:45 +0800

    change webqo,webqw urls paths

--- webqo/urls.py
+++ webqo/urls.py
@@ -27,15 +27,15 @@ app_name = 'webqo'
 urlpatterns = [
     # webqo
     re_path(r'^debug/$', views.debug),
-    re_path(r'^debug_del$', views.debug_del),
-    re_path(r'^debug_save$', views.debug_save),
-    re_path(r'^debug_diff$', views.debug_diff),
+    re_path(r'^debug/del$', views.debug_del),
+    re_path(r'^debug/save$', views.debug_save),
+    re_path(r'^debug/diff$', views.debug_diff),
     # re_path(r'^auto/$', views.auto),
-    re_path(r'^automation(?P<page_id>\d*)$',views.automation),
-    re_path(r'^automation_add',views.automation_add),
-    re_path(r'^automation_detail_(?P<task_id>\d+).html$',views.automation_detail),
-    re_path(r'^automation_restart$',views.automation_restart),
-    re_path(r'^automation_cancel',views.automation_cancel),
+    re_path(r'^auto(?P<page_id>\d*)$',views.auto),
+    re_path(r'^auto/add',views.auto_add),
+    re_path(r'^auto/auto_detail_(?P<task_id>\d+).html$',views.auto_detail),
+    re_path(r'^auto/restart$',views.auto_restart),
+    re_path(r'^auto/cancel',views.auto_cancel),
 
     # re_path(r'^logout$',views.logout),
 ]
--- webqw/urls.py
+++ webqw/urls.py
@@ -27,14 +27,14 @@ app_name = 'webqw'
 urlpatterns = [
     # webqw
     re_path(r'^debug/$', views.debug),
-    re_path(r'^debug_save', views.debug_save),
-    re_path(r'^debug_del', views.debug_del),
-    re_path(r'^debug_diff$', views.debug_diff),
-    re_path(r'^automation(?P<page_id>\d*)$', views.automation),
-    re_path(r'^automation_add$', views.automation_add),
-    re_path(r'^automation_detail_(?P<task_id>\d+).html$', views.automation_detail),
-    re_path(r'^automation_restart$', views.automation_restart),
-    re_path(r'^automation_cancel', views.automation_cancel),
+    re_path(r'^debug/save', views.debug_save),
+    re_path(r'^debug/del', views.debug_del),
+    re_path(r'^debug/diff$', views.debug_diff),
+    re_path(r'^auto(?P<page_id>\d*)$', views.auto),
+    re_path(r'^auto/add$', views.auto_add),
+    re_path(r'^auto/auto_detail_(?P<task_id>\d+).html$', views.auto_detail),
+    re_path(r'^auto/restart$', views.auto_restart),
+    re_path(r'^auto/cancel', views.auto_cancel),
     # re_path(r'^qw_req$',views.qw_req),
     # re_path(r'^qw_req_info$',views.qw_req_info),
 
