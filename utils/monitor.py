9d077628ca46e2149f0ceaa5f729f7b2f6b083de
zjjfx88 <zjj7842806@163.com>
2018-08-22 11:33:58 +0800

    update monitor

--- utils/monitor.py
+++ utils/monitor.py
@@ -35,6 +35,7 @@ def update_errorlog(log):
         db.commit()
     except:
         logInfo.log_info('Insert log error')
+        pass
 
 
 def set_status(stat):
@@ -90,31 +91,33 @@ def gpu_info(host_id):
     (host_ip,passw,gpuid,processname) = cursor.fetchone()
     if not processname.strip():
         while True:
-            command_line = "nvidia-smi | egrep -A 1 '"+ str(gpuid) +".*[PMK]40'| grep -v 'Tesla'"
-            child = ssh_command("root", host_ip, passw, command_line)
-            child.expect(pexpect.EOF)
-            gpuinfo = (child.before).decode('utf-8')
-            gpu_lst = gpuinfo.strip().split('\r\n')
-            g_mem=gpu_lst[0].split()[8].split('MiB')[0]
-            g_used=gpu_lst[0].split()[12].split('%')[0]
-            g_mem_list = g_mem + ","
-            g_used_list = g_used + ","
-            timedata = datetime.now().strftime('[Date.UTC(%Y,%m,%d,%H,%M,%S)')
-            gpumeminfo = timedata+","+g_mem+'],\n'
-            gpumemused = timedata+","+g_used+'],\n'
-            db = pymysql.connect(database_host, database_user, database_pass, database_data)
-            cursor = db.cursor()
-            sql = "UPDATE %s set gpumem=CONCAT(gpumem, '%s'),gpumemused=CONCAT(gpumemused, '%s') ,gpumem_list=CONCAT(gpumem_list, '%s'),gpumemused_list=CONCAT(gpumemused_list, '%s') where id=%d;" % (database_table, gpumeminfo, gpumemused,g_mem_list,g_used_list,monitor_id)
-            logInfo.log_info('gpu mem is '+g_mem+' and gpu mem used is '+g_used)
-            cursor.execute(sql)
             try:
-                db.commit()
-            except:
-                db.rollback()
-                update_errorlog("[%s] Insert a piece of data failed \n" % get_now_time())
-                logInfo.log_info('Insert a piece of data failed')
-                sys.exit()
-            time.sleep(5)
+                command_line = "nvidia-smi | egrep -A 1 '"+ str(gpuid) +".*[PMK]40'| grep -v 'Tesla'"
+                child = ssh_command("root", host_ip, passw, command_line)
+                child.expect(pexpect.EOF)
+                gpuinfo = (child.before).decode('utf-8')
+                gpu_lst = gpuinfo.strip().split('\r\n')
+                g_mem=gpu_lst[0].split()[8].split('MiB')[0]
+                g_used=gpu_lst[0].split()[12].split('%')[0]
+                g_mem_list = g_mem + ","
+                g_used_list = g_used + ","
+                timedata = datetime.now().strftime('[Date.UTC(%Y,%m,%d,%H,%M,%S)')
+                gpumeminfo = timedata+","+g_mem+'],\n'
+                gpumemused = timedata+","+g_used+'],\n'
+                db = pymysql.connect(database_host, database_user, database_pass, database_data)
+                cursor = db.cursor()
+                sql = "UPDATE %s set gpumem=CONCAT(gpumem, '%s'),gpumemused=CONCAT(gpumemused, '%s') ,gpumem_list=CONCAT(gpumem_list, '%s'),gpumemused_list=CONCAT(gpumemused_list, '%s') where id=%d;" % (database_table, gpumeminfo, gpumemused,g_mem_list,g_used_list,monitor_id)
+                logInfo.log_info('gpu mem is '+g_mem+' and gpu mem used is '+g_used)
+                cursor.execute(sql)
+                try:
+                    db.commit()
+                except:
+                    db.rollback()
+                    update_errorlog("[%s] Insert a piece of data failed \n" % get_now_time())
+                    logInfo.log_info('Insert a piece of data failed')
+                    pass
+                time.sleep(5)
+            except Exceptions
     else:
         while True:
             command_line_one = "nvidia-smi | egrep -A 1 '"+ str(gpuid) +".*[PMK]40'| grep -v 'Tesla'"
@@ -150,10 +153,9 @@ def gpu_info(host_id):
                 db.commit()
             except:
                 db.rollback()
-                set_status(2)
                 update_errorlog("[%s] Insert a piece of data failed \n" % get_now_time())
                 logInfo.log_info('Insert a piece of data failed')
-                sys.exit()
+                pass
             time.sleep(5)
 
 
