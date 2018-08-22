52efeea44327349fc3ae80181af4a8f407cc042e
zjjfx88 <zjj7842806@163.com>
2018-08-22 19:02:21 +0800

    change reqtext long

--- fanyi/models.py
+++ fanyi/models.py
@@ -6,7 +6,7 @@ class ReqInfo(models.Model):
     host_ip = models.CharField(max_length=128)
     trans_direct = models.CharField(max_length=20)
     isfromzh = models.CharField(max_length=10)
-    req_text = models.CharField(max_length=2000)
+    req_text = models.TextField(default="")
     result = models.CharField(max_length=2000)
     user_fk = models.ForeignKey(to=UserInfo,to_field='username',on_delete=models.CASCADE)
     reqtype = models.CharField(max_length=20)
