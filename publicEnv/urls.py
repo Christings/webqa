83552df78131a1fbc67487d7d37ee1f82a46cfa2
zjjfx88 <zjj7842806@163.com>
2018-11-27 11:46:17 +0800

    modify

---
+++ publicEnv/urls.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'zhangjingjun'
+__mtime__ = '2018/8/6'
+# ----------Dragon be here!----------
+              ┏━┓      ┏━┓
+            ┏━┛ ┻━━━━━━┛ ┻━━┓
+            ┃       ━       ┃
+            ┃  ━┳━┛   ┗━┳━  ┃
+            ┃       ┻       ┃
+            ┗━━━┓      ┏━━━━┛
+                ┃      ┃神兽保佑
+                ┃      ┃永无BUG！
+                ┃      ┗━━━━━━━━━┓
+                ┃                ┣━┓
+                ┃                ┏━┛
+                ┗━━┓ ┓ ┏━━━┳━┓ ┏━┛
+                   ┃ ┫ ┫   ┃ ┫ ┫
+                   ┗━┻━┛   ┗━┻━┛
+"""
+from django.urls import re_path
+from . import views
+app_name = 'publicEnv'
+urlpatterns = [
+    # auto Bleu
+    re_path(r'^public/deadlink$', views.deadlink),
+    re_path(r'^public/svcheck$', views.svcheck),
+]
