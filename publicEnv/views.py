5b0dce939e5a558f6c22d3de6d57dd322e097c4d
zjjfx88 <zjj7842806@163.com>
2018-11-29 09:28:15 +0800

    add page

--- publicEnv/views.py
+++ publicEnv/views.py
@@ -46,40 +46,58 @@ def svcheck_detail(request):
 
 @auth
 def svcheck(request):
-    # user_id = 'zhangjingjun'
-    user_id = request.COOKIES.get('uid')
+    user_id = 'zhangjingjun'
+    # user_id = request.COOKIES.get('uid')
     if request.method == 'GET':
         tag = request.GET.get('tag')
         search_content = request.GET.get('key')
+        # try:
+        #     if search_content is not None and search_content !='':
+        #         task_list = models.ServiceStatus.objects.filter(
+        #             Q(sv_name__icontains=search_content) | Q(sv_host__icontains=search_content)
+        #             | Q(sv_port__icontains=search_content) | Q(svninfo__icontains=search_content)
+        #             | Q(sv_path__icontains=search_content) | Q(host_online__icontains=search_content)
+        #             | Q(path_online__icontains=search_content)).order_by('status')
+        #     elif tag is None or tag == 'all':
+        #         task_list = models.ServiceStatus.objects.all().order_by('status')
+        #     elif tag == 'crash':
+        #         task_list = models.ServiceStatus.objects.filter(status=0).order_by('id')
+        #     elif tag == 'unonline':
+        #         task_list = models.ServiceStatus.objects.filter(host_online='').order_by('status')
+        # except Exception as e:
+        #     print(e)
+        #     pass
+        # return render(request, 'publicsv/svcheck.html',{'user_id': user_id,'task_list': task_list})
+        page = request.GET.get('page')
+        current_page = 1
+        if page:
+            current_page = int(page)
+        if tag:
+            tag = tag
+        else:
+            tag = ""
+        if search_content:
+            search_content = search_content
+        else:
+            search_content = ""
+        print('tag:',tag,' searchcount:',search_content,' page:',page)
         try:
-            if search_content is not None and search_content !='':
+            if search_content  and search_content != '':
                 task_list = models.ServiceStatus.objects.filter(
                     Q(sv_name__icontains=search_content) | Q(sv_host__icontains=search_content)
                     | Q(sv_port__icontains=search_content) | Q(svninfo__icontains=search_content)
                     | Q(sv_path__icontains=search_content) | Q(host_online__icontains=search_content)
                     | Q(path_online__icontains=search_content)).order_by('status')
-            elif tag is None or tag == 'all':
+            elif tag=='' or tag == 'all':
                 task_list = models.ServiceStatus.objects.all().order_by('status')
             elif tag == 'crash':
                 task_list = models.ServiceStatus.objects.filter(status=0).order_by('id')
             elif tag == 'unonline':
                 task_list = models.ServiceStatus.objects.filter(host_online='').order_by('status')
+            page_obj = pagination.Page(current_page, len(task_list), 28, 9)
+            data = task_list[page_obj.start:page_obj.end]
+            page_str = page_obj.page_str("/publicsv/svcheck?tag=%s&key=%s&page=" % (tag,search_content))
         except Exception as e:
             print(e)
             pass
-        return render(request, 'publicsv/svcheck.html',{'user_id': user_id,'task_list': task_list})
-        # page = request.GET.get('page')
-        # tag = request.GET.get('tag')
-        # print(type(tag))
-        # current_page = 1
-        # if page:
-        #     current_page = int(page)
-        # try:
-        #     task_list = models.ServiceStatus.objects.order_by('status')
-        #     page_obj = pagination.Page(current_page, len(task_list), 16, 9)
-        #     data = task_list[page_obj.start:page_obj.end]
-        #     page_str = page_obj.page_str("/publicsv/svcheck?page=")
-        # except Exception as e:
-        #     print(e)
-        #     pass
-        # return render(request, 'publicsv/svcheck.html', {'user_id': user_id, 'li': data, 'page_str': page_str,'task_list':task_list})
+        return render(request, 'publicsv/svcheck.html', {'user_id': user_id, 'li': data, 'page_str': page_str,'task_list':task_list})
