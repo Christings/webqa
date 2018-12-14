f28bf909c46893a9bd7bfd60b7829ee3e8af5322
zjjfx88 <zjj7842806@163.com>
2018-12-14 14:26:40 +0800

    add celery

---
+++ webqa/celery.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'zhangjingjun'
+__mtime__ = '2018/12/14'
+# ----------Dragon be here!----------
+              ┏━┓      ┏━┓
+            ┏━┛ ┻━━━━━━┛ ┻━━┓
+            ┃       ━       ┃
+            ┃  ━┳━┛   ┗━┳━  ┃
+            ┃       ┻       ┃
+            ┗━━━┓      ┏━━━━┛
+                ┃      ┃神兽保佑
+                ┃      ┃永无BUG！
+                ┃      ┗━━━━━━━━━┓
+                ┃                ┣━┓
+                ┃                ┏━┛
+                ┗━━┓ ┓ ┏━━━┳━┓ ┏━┛
+                   ┃ ┫ ┫   ┃ ┫ ┫
+                   ┗━┻━┛   ┗━┻━┛
+"""
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery,platforms
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webqa.settings')
+
+from django.conf import settings  # noqa
+
+platforms.C_FORCE_ROOT = True
+app = Celery('webqa')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
