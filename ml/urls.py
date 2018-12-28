0d30f1b52c5ecdeb2dba5e06012021642e1730df
Gladysgong <531404376@qq.com>
2018-12-28 17:31:17 +0800

    ml:urls

--- ml/urls.py
+++ ml/urls.py
@@ -33,15 +33,11 @@ router.register(r'rule', views.RuleViewSet)
 router.register(r'field', views.FieldViewSet)
 
 urlpatterns = [
-
     # crawler
-    # re_path(r'^project', views.crawler_list),
     re_path(r'^project/add', views.crawler_add),
     re_path(r'^project/edit/(?P<id>\d+)$', views.crawler_edit),
-    re_path(r'^project/run/(?P<id>\d+)$', views.crawler_run),
-    re_path(r'^project_status/(?P<id>\d+)$', views.crawler_detail),
-
-    # re_path(r'^project', include(router.urls)),
+    # re_path(r'^project/run/(?P<id>\d+)$', views.crawler_run),
+    #
     re_path(r'^', include(router.urls)),
     re_path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
